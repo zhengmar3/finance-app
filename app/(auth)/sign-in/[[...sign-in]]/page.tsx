17231968e4320675fd5b7e97111ba2eb0d5ad6a2
Zheng Ma <zheng.ma@r3.com>
2024-05-21 12:10:39 +0100

    sign in & up

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import { Loader2 } from "lucide-react";
+import { SignIn, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="h-full lg:flex flex-col items-center justify-center px-4">
+        <div className="text-center space-y-4 pt-16">
+          <h1 className="font-bold text-3xl text-[#2E2A47]">Welcome Back</h1>
+          <p className="text-base text-[#4F4C67]">
+            Sign in to your account to continue!
+          </p>
+        </div>
+        <div className="flex items-center justify-center mt-8">
+          <ClerkLoaded>
+            <SignIn path="/sign-in" />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="animate-spin text-muted-foreground" />
+          </ClerkLoading>
+        </div>
+      </div>
+      <div className="h-full hidden bg-blue-600 lg:flex items-center justify-center">
+        <Image src="logo.svg" height={100} width={100} alt="Logo" />
+      </div>
+    </div>
+  );
+}
